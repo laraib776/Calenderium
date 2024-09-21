dbf493272fd8c91ed16f626df863dd58ea1b66aa
ShibalSaekiya ^-^ <laraib776>
2024-09-20 23:14:45 -0700

    Add .gitignore file

---
+++ myclub_website/wsgi.py
@@ -0,0 +1,8 @@
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myclub_website.settings')
+
+application = get_wsgi_application()
