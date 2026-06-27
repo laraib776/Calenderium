130f8eecf9953657e7d168bd1dc5706f899506f9
Laraib Khalid <120477982+laraib776@users.noreply.github.com>
2026-06-27 20:03:54 +0500

    Admin Event Form forms.py

--- events/forms.py
+++ events/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.forms import ModelForm
 from .models import Venue, Event
 
-# Admin SuperUser Event Form
-class EventFormAdmin(ModelForm):
+# Admin SuperUser Event Form]
+# class EventFormAdmin(ModelForm):class EventFormAdmin(ModelForm):
 	class Meta:
 		model = Event
 		fields = ('name', 'event_date', 'venue', 'manager', 'attendees', 'description')
